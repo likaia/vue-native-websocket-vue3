a154d5336363913685f45a710d85e3f081316a3d
likaia <1195419506@qq.com>
2021-01-14 21:13:23 +0800

    fix: update .gitignore

--- dist/pluginsType.js
+++
@@ -1,3 +0,0 @@
-// 插件内用到的类型进行统一定义
-export {};
-//# sourceMappingURL=pluginsType.js.map
--- dist/types/Main.d.ts
+++
@@ -1,6 +0,0 @@
-import { websocketOpts } from "src/pluginsType";
-import { App } from "vue";
-declare const _default: {
-    install(app: App, connection: string, opts?: websocketOpts): void;
-};
-export default _default;
